8dc129932647776c72b08862379c49bf5e73675b
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-22 21:41:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -85,7 +85,7 @@ def organize_schools(schools)
   
   school_locations =  [    ]
   schools.each do |element, attribute|
-    puts attribute.keys
+    puts attribute.keys.class
     if attribute.keys == :location
       school_locations[attribute.values] = [ ]
 
